bdfbfde11a83ba436017f8bc286af5d6603b6834
AsbjornWestvik <asbjorn.westvik@gmail.com>
2023-09-09 08:39:16 +0200

    further work on own file plus a solution

--- sudokoSolver.py
+++ sudokoSolver.py
@@ -15,30 +15,6 @@ board = [[0,0,8,0,0,0,2,3,0],
 for i in board:
     print(i)
 
-print("New version")
-    
-for a in range(9):
-    print("[",end="")
-    for b in range(9):
-        # This line prints without newline at end
-        print(str(board[a][b]) + ", ",end="")
-    print("]")
-
-
-# Define which quadrant the slots belong to, defined as such:
-#  A, B, C    =   0, 1, 2
-#  D, E, F    =   3, 4, 5
-#  G, H, I    =   6, 7, 8
-
-for a in range(9):
-    for b in range(9):
-        row = int(a/3)*3
-        col = int(b/3)
-        print(row+col,end="")
-    print()
-
-
-
 # Create a 3 level list, potential numbers in each position
 # Level 1: All positions can have 1 to 9
 
@@ -85,32 +61,40 @@ for b in range(9):
 
 
 # Level 5: Remove numbers which are in 3x3 square
-miniBoard = [[[1, 2, 3, 4, 5, 6, 7, 8, 9],[1, 2, 3, 4, 5, 6, 7, 8, 9],8],
-             [[1, 2, 3, 4, 5, 6, 7, 8, 9],[1, 2, 3, 4, 5, 6, 7, 8, 9],[1, 2, 3, 4, 5, 6, 7, 8, 9]],
-             [7,[1, 2, 3, 4, 5, 6, 7, 8, 9],9]]
+for i in range(0,9,3):
+    for j in range(0,9,3):
+        columnList = []
+        for a in range(3):
+            for b in range(3):
+                currentA = (int)(i/3)*3+a
+                currentB = (int)(j/3)*3+b
+                currentValue = potentialNumbers[currentA][currentB]
+                if type(currentValue) == int:
+                    columnList.append(currentValue)
+        for a in range(3):
+            for b in range(3):
+                currentA = (int)(i/3)*3+a
+                currentB = (int)(j/3)*3+b
+                currentValue = potentialNumbers[currentA][currentB]
+                if type(currentValue) == list:
+                    potentialNumbers[currentA][currentB] = [k for k in currentValue if k not in columnList]
+                
+                
+for a in range(9):
+    for b in range(9):
+        print(potentialNumbers[a][b])
 
-print("miniBoard")
-print(miniBoard)
-
-print("inside for loops")
-for b in range(3):
-    columnList = []
-    for a in range(3):
-        currentValue = miniBoard[a][b]
-        if type(currentValue) == int:
-            columnList.append(currentValue)
-    for a in range(3):
-        currentValue = miniBoard[a][b]
-        if type(currentValue) == list:
-            miniBoard[a][b] = [i for i in currentValue if i not in columnList]
-
-print(miniBoard)
+def solve(boardToFill):
+    for i in range(9):
+        for b in range(9):
+            print(potentialNumbers[a][b])
+            if (type(potentialNumbers[a][b]) == int):
+                boardToFill.append(potentialNumbers[a][b])
+            else:
+                boardToFill.append(0)
+                
+finalBoard = []
+solve(finalBoard)
+print(finalBoard)
 
 
-
-print("new testStuff")
-list1 = [1,2,3,4,5,6,7]
-list2 = [3,5,6]
-
-removedList = [i for i in list1 if i not in list2]
-print(removedList)
